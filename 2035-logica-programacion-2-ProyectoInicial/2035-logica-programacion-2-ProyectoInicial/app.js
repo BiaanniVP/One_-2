57cd7d12dcf2b7d048ff42abd1a7530be468ca60
Biaanni <107739319+BiaanniVP@users.noreply.github.com>
2025-09-06 15:23:20 -0600

    Update app.js

---
+++ 2035-logica-programacion-2-ProyectoInicial/2035-logica-programacion-2-ProyectoInicial/app.js
@@ -0,0 +1,6 @@
+  //conectar xon el htlm
+let titulo= document.querySelector('h1'); //regresa titulo
+titulo.innerHTLM='Juego del número secreto';
+
+let parrafo=document.querySelector('p');
+parrafo.innerHTLM = 'Indica un númerod el 1 al 10";
