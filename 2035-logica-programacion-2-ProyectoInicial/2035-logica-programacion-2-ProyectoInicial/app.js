baf205b5416ef5bdb0a8780ddd57a146d8727966
Biaanni <107739319+BiaanniVP@users.noreply.github.com>
2025-09-06 16:19:40 -0600

    Update app.js

--- 2035-logica-programacion-2-ProyectoInicial/2035-logica-programacion-2-ProyectoInicial/app.js
+++ 2035-logica-programacion-2-ProyectoInicial/2035-logica-programacion-2-ProyectoInicial/app.js
@@ -1,8 +1,24 @@
-  //conectar xon el htlm
-let titulo= document.querySelector('h1'); //regresa titulo
-titulo.innerHTLM='Juego del número secreto';
+let numeroSecreto = generarNumeroSecreto; 
 
-let parrafo=document.querySelector('p');
-parrafo.innerHTLM = 'Indica un númerod el 1 al 10";
+/* para ver si esta funcionando
+console.log(numeroSecreto); */
 
-//interactuar con un boton
+
+//interactuar con un boton y conectar con htlm con funciones 
+function asignarTextoElemento(elemento,texto){
+  let elementoHTLM= document.querySelector('h1'); //regresa titulo
+  elementoHTLM.innerHTLM=texto;
+  return;//buena practica 
+}
+function verificarIntento (){
+  alert (´Click desde el boton');
+}//funcion
+
+function generarNumeroSecreto(){
+  let numeroSecreto =Math.floor ( Math.ramdom()*10)+1;
+  return numeroSecreto; 
+}
+
+//Puedo llamar la funcion desde el codigo js SOLO en eventos 
+asignarTextoElemento( 'h1','Juego del número secreto');
+asignarTextoElemento( 'p','Indica un número del 1 al 10');
