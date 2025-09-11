e447136ab80b95e7cd33d86a1edb2e3d38bf5387
Biaanni <107739319+BiaanniVP@users.noreply.github.com>
2025-09-10 20:58:04 -0600

    Update app.js

--- 2035-logica-programacion-2-ProyectoInicial/2035-logica-programacion-2-ProyectoInicial/app.js
+++ 2035-logica-programacion-2-ProyectoInicial/2035-logica-programacion-2-ProyectoInicial/app.js
@@ -1,5 +1,5 @@
 let numeroSecreto = generarNumeroSecreto; 
-
+let intentos = 1;
 /* para ver si esta funcionando
 console.log(numeroSecreto); */
 
@@ -11,16 +11,28 @@ function asignarTextoElemento(elemento,texto){
   return;//buena practica 
 }
 function verificarIntento (){
-  let numeroDe Usuario = document.querySelector('input');
+  let numeroDeUsuario = document.querySelector('input');
+
+  if(numeroDeUsuario === numeroSecreto){
+    asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos ===1) ? 'vez' : 'veces?'}`)
+  } else {
+    if (numeroDeUsuario > numeroSecreto) {
+      asignarTextoElemento ( 'p', '¡El número secreto es menor!');
+    }  else {
+    if (numeroDeUsuario < numeroSecreto) {
+      asignarTextoElemento ( 'p', '¡El número secreto es mayor!');
+    }
+      intentos++;//contador 
   return;
 }//funcion
 
 function generarNumeroSecreto(){
-  let numeroSecreto =Math.floor ( Math.ramdom()*10)+1;
+  let numeroSecreto =Math.floor ( Math.ramdom()*20)+1;
   return numeroSecreto; 
 }
 
 //Puedo llamar la funcion desde el codigo js SOLO en eventos 
 asignarTextoElemento( 'h1','Juego del número secreto');
 asignarTextoElemento( 'p','Indica un número del 1 al 10');
+
 
